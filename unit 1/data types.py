1c719bf3026329b8801a1b25227f3591d6eecb07
alijahs21 <asmith509@boyslatin.net>
2025-08-22 12:46:05 +0000

    class review

---
+++ unit 1/data types.py
@@ -0,0 +1,18 @@
+#strings
+
+"this is a string"
+
+"Alijah Smith"
+
+"this is also a string"
+
+"211325"
+
+# this is a message that only humans can see 
+# it is invisle to the computer
+
+#intergers
+
+13131313
+2121111
+555555
